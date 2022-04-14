57506006315fde57c375b8b365f72a3d81903c04
escoobi <gustavo_o_@hotmail.com>
2022-04-13 23:40:53 -0400

    Usando Blueprint

---
+++ run.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from pessoas.run_pessoas import app_pessoas
+
+app = Flask(__name__)
+app.register_blueprint(app_pessoas)
+
+@app.route("/")
+def index():
+    return f"Page one!!!"
+
+if __name__ == "__main__":
+    app.run(debug=True, port=6289)
