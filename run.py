1eb1a719a4a792f7a8fd38822f14cc2f84531754
escoobi <gustavo_o_@hotmail.com>
2022-04-17 22:58:54 -0400

    Documentando API

--- run.py
+++ run.py
@@ -1,12 +1,17 @@
-from flask import Flask
-from view.pessoas.run_pessoas import app_pessoas
+from flask import Flask, jsonify
+from run_pessoas import app_pessoas
+from flask_restx import Api, Resource
 
+
+api = Api()
 app = Flask(__name__)
 app.register_blueprint(app_pessoas)
+api.init_app(app)
 
-@app.route("/")
-def index():
-    return f"Page one!!!"
+@api.route("/")
+class HelloWorld(Resource):
+    def get(self):
+        return {"hello": "123"}
 
 if __name__ == "__main__":
-    app.run(debug=True, port=6289)
+    app.run(debug=True)
